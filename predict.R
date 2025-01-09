7749e015e7b6309927a84d690e988fdef54343b9
Knut Dagestad Rand <knutdrand@gmail.com>
2025-01-09 15:56:13 +0100

    Update predict.R

--- predict.R
+++ predict.R
@@ -41,3 +41,13 @@ predict_chap <- function(model_fn, historic_data_fn, future_climatedata_fn, pred
   }
   write.csv(full_df, predictions_fn, row.names = FALSE)
 }
+args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args) == 4) {
+  model_fn <- args[1]
+  historic_data_fn <- args[2]
+  future_climatedata_fn <- args[3]
+  predictions_fn <- args[4]
+  
+  predict_chap(model_fn, historic_data_fn, future_climatedata_fn, predictions_fn)
+}
